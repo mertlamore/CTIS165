69c9ebc6202715239b20a7e5d54d24ce10355ad1
21702033 <21702033@LAB113>
2018-03-14 16:15:33 +0300

    Change Pistons

--- SuzukiGSX1300Hayabusa/TR011/Hayabusa's200hpEngine.cpp
+++ SuzukiGSX1300Hayabusa/TR011/Hayabusa's200hpEngine.cpp
@@ -1,6 +1,10 @@
-#include <stdio.h>
-int main(void)
-{
-	printf("I am a bumblebee \n");
-	return(0);
+#include <stdio.h>  
+int main(void) {
+	int i, j, k;
+	for (i = 0; i < 8; i++) {
+		for (k = 7; k >= i; k--)    printf(" ");
+		for (j = 0; j <= i; j++)    printf("*");
+		printf("\n");
+	}
+	return 0;
 }
